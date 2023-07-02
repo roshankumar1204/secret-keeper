02f6fb6f1685263b99e753a8b1b4031a0ba2024a
Roshan kumar <roshankumarskr@gmail.com>
2023-07-02 13:33:58 +0530

    2nd lvl secerity

---
+++ app.js
@@ -0,0 +1,69 @@
+//jshint esversion:6
+ const express = require("express");
+ const bodyParser= require("body-parser");
+ const ejs =require("ejs");
+ const mongoose= require("mongoose");
+ const encrypt =require("mongoose-encryption");
+
+
+ const app= express();
+
+ app.use(express.static("public"));
+ app.set('view engine','ejs')
+ app.use(bodyParser.urlencoded({
+    extended:true}));
+
+mongoose.connect("mongodb://127.0.0.1/userDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema({
+    email:String,
+    password:String 
+})
+
+const secret = "thisissecretbro";
+userSchema.plugin(encrypt,{secret:secret ,encryptedFields:['password']});
+
+const User = mongoose.model("User",userSchema);
+
+app.get("/",function(req,res){
+    res.render("home");
+})
+
+app.get("/login",function(req,res){
+    res.render("login");
+})
+
+app.get("/register",function(req,res){
+    res.render("register");
+})
+
+app.post("/register",function(req,res){
+
+    const newUser = new User({
+        email:req.body.username,
+        password:req.body.password
+    })
+
+    newUser.save().then(function(){
+        res.render("secrets")
+    }).catch(function(error){ 
+        res.send(error);
+    })
+})
+app.post("/login", function(req,res){
+    const username= req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ email: username}).then((docs)=>{
+            if(docs.password === password){
+                res.render("secrets")
+            }
+    }).catch(function(error){
+        res.send(error);
+    })
+});
+
+
+app.listen(3000,function(){
+    console.log("serever running on 3000");
+});
